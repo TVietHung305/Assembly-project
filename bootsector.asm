4010facb8839552178a9edaf9318ee91e8453f63
IceK <120624208+TVietHung305@users.noreply.github.com>
2025-02-19 01:02:45 +0700

    Update bootsector.asm

--- bootsector.asm
+++ bootsector.asm
@@ -1,27 +1,27 @@
-org 0x7c00
-dw 0xaa55
+org 0x7c00]
 
 mov bp, 0xffff
 mov sp, bp
 
-call video_mode
-call input
+call set_video_mode
+call get_char_input
 
 jmp $
 
-video_mode:
+set_video_mode:
 	mov ah, 0x00
 	mov al, 0x03
 	int 0x10
 	ret
 
-input:
-	xor ah, ah 		
+get_char_input:
+	xor ah, ah 		; same as mov ah, 0x00
 	int 0x16
 
 	mov ah, 0x0e
 	int 0x10
 
-	jmp input
+	jmp get_char_input
 
 times 0x1fe-($-$$) db 0
+dw 0xaa55
